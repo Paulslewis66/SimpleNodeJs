4f86c36e23c23767b997466b569c5b6fc27195a0
Lewis <LEWIS3@science.regn.net>
2020-12-27 21:26:45 +0000

    all

--- firstWebserver.js
+++ firstWebserver.js
@@ -5,4 +5,4 @@ http.createServer(function (req, res) {
     res.end('Hello World from a Node.js webserver\n');
 }).listen(3000, '127.0.0.1');
 
-console.log('The server is up and running at http://127.0.0.1:3000/');
+console.log('The server is up and running at http://127.0.0.1:8000/');
