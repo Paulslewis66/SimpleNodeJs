d51655d35f4f641cc36ea28eaef87b1087982d5d
Lewis <LEWIS3@science.regn.net>
2020-12-28 18:13:38 +0000

    bh

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World from a Node.js webserver\n');
+    res.send('Hello World from a Node.js webserver\n');
 }).listen(8000, '127.0.0.1');
 
 console.log('The server is up and running at http://127.0.0.1:8000/');
